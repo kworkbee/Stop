28941ee007747488d16d64a717c34026116d8e5a
G1 <g1.jeon@kakao.com>
2021-11-19 17:31:17 +0900

    init: Initial Commit (SPA (Vue.js) + Springboot Boilerplate)

---
+++ src/main/java/com/tommy/g1/whatareyoudoing/WhatAreYouDoingApplication.java
@@ -0,0 +1,13 @@
+package com.tommy.g1.whatareyoudoing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatAreYouDoingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatAreYouDoingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tommy/g1/whatareyoudoing/WhatAreYouDoingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tommy.g1.whatareyoudoing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatAreYouDoingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
