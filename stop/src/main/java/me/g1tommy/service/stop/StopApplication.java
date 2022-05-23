9ffb3f6285e922e167f049b2bc3afab69b336d92
G1 <g1.tommy.jeon@gmail.com>
2022-05-24 00:24:34 +0900

    feat: Initial Commit

---
+++ stop/src/main/java/me/g1tommy/service/stop/StopApplication.java
@@ -0,0 +1,13 @@
+package me.g1tommy.service.stop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StopApplication.class, args);
+	}
+
+}
---
+++ stop/src/test/java/me/g1tommy/service/stop/StopApplicationTests.java
@@ -0,0 +1,13 @@
+package me.g1tommy.service.stop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
